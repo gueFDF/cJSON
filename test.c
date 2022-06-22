0dba5ad79721e473c7966d173ffdd002cdf25cb9
gueFDF <3237455241@qq.com>
2022-06-22 16:37:18 +0800

    第一单元练习

--- test.c
+++ test.c
@@ -25,9 +25,49 @@ static void test_parse_null() {
     EXPECT_EQ_INT(LEPT_PARSE_OK, lept_parse(&v, "null"));
     EXPECT_EQ_INT(LEPT_NULL, lept_get_type(&v));
 }
+static void test_parse_expect_value()
+{
+    lept_value v;
+    v.type=LEPT_FALSE;
+    EXPECT_EQ_INT(LEPT_PARSE_EXPECT_VALUE,lept_parse(&v,""));
+    EXPECT_EQ_INT(LEPT_NULL,lept_get_type(&v));
+
+    v.type=LEPT_FALSE;
+    EXPECT_EQ_INT(LEPT_PARSE_EXPECT_VALUE,lept_parse(&v," "));
+    EXPECT_EQ_INT(LEPT_NULL,lept_get_type(&v));
+}
+static void test_parse_invalid_valie()
+{
+    lept_value v;
+    v.type=LEPT_FALSE;
+    EXPECT_EQ_INT(LEPT_PARSE_INVALID_VALUE,lept_parse(&v,"nul"));
+    EXPECT_EQ_INT(LEPT_NULL,lept_get_type(&v));
+     
+    v.type=LEPT_FALSE;
+    EXPECT_EQ_INT(LEPT_PARSE_INVALID_VALUE,lept_parse(&v,"?"));
+    EXPECT_EQ_INT(LEPT_NULL,lept_get_type(&v));
+}
+static void test_pares_root_not_singular()
+{
+    lept_value v;
+    v.type=LEPT_FALSE;
+    EXPECT_EQ_INT(LEPT_PARSE_ROOT_NOT_SINGULAR,lept_parse(&v,"null x"));
+    EXPECT_EQ_INT(LEPT_NULL,lept_get_type(&v));
+
+    v.type=LEPT_FALSE;
+    EXPECT_EQ_INT(LEPT_PARSE_ROOT_NOT_SINGULAR,lept_parse(&v,"null         x"));
+    EXPECT_EQ_INT(LEPT_NULL,lept_get_type(&v));
+
+    v.type=LEPT_FALSE;
+    EXPECT_EQ_INT(LEPT_PARSE_OK,lept_parse(&v,"null        "));
+    EXPECT_EQ_INT(LEPT_NULL,lept_get_type(&v));
+}
 static void test_parse()
 {
-    test_parse_null();
+ //   test_parse_null();
+    test_parse_expect_value();
+    test_parse_invalid_valie();
+    test_pares_root_not_singular();
 }
 int main() {
     test_parse();
