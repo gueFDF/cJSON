28cdc907a7e4297e84cf9ec9d0ea999779fe5ae5
gueFDF <3237455241@qq.com>
2022-06-22 20:48:48 +0800

    第二单元练习

--- leptjson.c
+++ leptjson.c
@@ -1,5 +1,8 @@
 #include"leptjson.h"
 #define EXPEXT(c,ch)  do{assert(*c->json==(ch));c->json++;}while(0)
+
+#define ISDIGIT(ch)         ((ch) >= '0' && (ch) <= '9')
+#define ISDIGIT1TO9(ch)     ((ch) >= '1' && (ch) <= '9')
 static void lept_parse_whitespace(lept_context* c)
 {
     const char* p=c->json;
@@ -9,15 +12,53 @@ static void lept_parse_whitespace(lept_context* c)
     }
     c->json=p;
 }
-static int lept_parse_number(lept_context* c, lept_value* v)
-{
-    char*end;
-    v->n=strtod(c->json,&end);
+/*static int lept_parse_number(lept_context* c, lept_value* v)
+{ p++;
+        if (!ISDIGIT(*p)) return LEPT_PARSE_INVALID_VALUE;
+        for (p++; ISDIGIT(*p); p++);
+    }
+    if (*p == 'e' || *p == 'E') {);
     if(c->json==end)
     return LEPT_PARSE_INVALID_VALUE;
     c->json=end;
     v->type=LEPT_NUMBER;
     return LEPT_PARSE_OK;
+}*/
+static int lept_parse_number(lept_context*c,lept_value*v)
+{
+    const char *p=c->json;
+    if(*p=='-') p++;
+    if(*p=='0') p++;
+    else
+    {
+        if(!ISDIGIT1TO9(*p))
+        return LEPT_PARSE_INVALID_VALUE;
+        for(p++;ISDIGIT(*p);p++);
+    }
+    if(*p=='.')
+    {
+        p++;
+        if(!ISDIGIT(*p))
+        return LEPT_PARSE_INVALID_VALUE;
+        for(p++;ISDIGIT(*p);p++);
+    }
+    if(*p=='e'||*p=='E')
+    {
+        p++;
+        if(*p=='-'||*p=='+')
+        p++;
+        if(!ISDIGIT(*p))
+        return LEPT_PARSE_EXPECT_VALUE;
+        for(p++;ISDIGIT(*p);p++);
+    }
+    errno=0;
+    v->n=strtod(c->json,NULL);
+    if(errno==ERANGE&&(v->n==HUGE_VAL||v->n==-HUGE_VAL))\
+        return LEPT_PARSE_NUMBER_TOO_BIG;
+    v->type=LEPT_NUMBER;
+    c->json=p;
+    return LEPT_PARSE_OK;
+
 }
 static int lept_parse_literal(lept_context* c, lept_value* v, const char* literal, lept_type type) 
 {
@@ -68,7 +109,10 @@ int lept_parse(lept_value* v, const char* json)
     {
         lept_parse_whitespace(&c);
         if(*c.json!='\0')
-        return LEPT_PARSE_ROOT_NOT_SINGULAR;
+        {
+            v->type=LEPT_NULL;
+            ret=LEPT_PARSE_ROOT_NOT_SINGULAR;
+        }
     }
     return ret; 
 }
--- leptjson.h
+++ leptjson.h
@@ -4,7 +4,8 @@
 #include<assert.h>
 #include<string.h>
 #include<stdlib.h>
-
+#include<errno.h>
+#include<math.h>
 typedef struct 
 {
     const char* json;
@@ -16,7 +17,8 @@ enum {
     LEPT_PARSE_OK = 0,     //正确
     LEPT_PARSE_EXPECT_VALUE,   //若一个 JSON 只含有空白
     LEPT_PARSE_INVALID_VALUE,   //若值不是那三种字面值
-    LEPT_PARSE_ROOT_NOT_SINGULAR  //若一个值之后，在空白之后还有其他字符
+    LEPT_PARSE_ROOT_NOT_SINGULAR,  //若一个值之后，在空白之后还有其他字符
+     LEPT_PARSE_NUMBER_TOO_BIG    //太大
 };
 typedef struct
 {
@@ -28,4 +30,5 @@ int lept_parse(lept_value* v, const char* json);   //解析json
 lept_type lept_get_type(const lept_value* v); //获取访问结果
 double lept_get_number(const lept_value* v);//获取数字
 static int lept_parse_number(lept_context* c, lept_value* v);
+static int lept_parse_literal(lept_context* c, lept_value* v, const char* literal, lept_type type) ;
 #endif 
