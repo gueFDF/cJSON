e42baf0b31a4f40b99c6d9065f1a6b3499b8c069
gueFDF <3237455241@qq.com>
2022-06-22 17:33:20 +0800

    优化了测试

--- test.c
+++ test.c
@@ -5,69 +5,69 @@
 static int main_ret=0;
 static int test_count=0;
 static int test_pass=0;
-#define EXPECT_EQ_BASE(equality, expect, actual, format)\
+#define EXPECT_EQ_BASE(equality, expect, actual, format) \
     do {\
         test_count++;\
-        if(equality)\
-        test_pass++;\
-        else\
-        {\
-            fprintf(stderr,"%s:%d:expect:"format"actual:"format"\n",__FILE__, __LINE__,expect,actual);\
-            main_ret=1;\
+        if (equality)\
+            test_pass++;\
+        else {\
+            fprintf(stderr, "%s:%d: expect: " format " actual: " format "\n", __FILE__, __LINE__, expect, actual);\
+            main_ret = 1;\
         }\
-    }while(0)
+    } while(0)
+
 
 #define EXPECT_EQ_INT(expect,actual) EXPECT_EQ_BASE(((expect)==(actual)),expect,actual,"%d")
 
-static void test_parse_null() {
+
+
+#define TESTERROR(error,json)\
+    do{\
+        lept_value v;\
+        v.type=LEPT_FALSE;\
+        EXPECT_EQ_INT(error,lept_parse(&v,json));\
+        EXPECT_EQ_INT(LEPT_NULL,lept_get_type(&v));\
+    }while(0)
+
+static void test_parse_error()
+{
+    TESTERROR(LEPT_PARSE_INVALID_VALUE,"nul");
+    TESTERROR(LEPT_PARSE_INVALID_VALUE,"?");
+    TESTERROR(LEPT_PARSE_EXPECT_VALUE,"");
+    TESTERROR(LEPT_PARSE_EXPECT_VALUE," ");
+    TESTERROR(LEPT_PARSE_OK,"null       ");
+    TESTERROR(LEPT_PARSE_ROOT_NOT_SINGULAR,"null x");
+    TESTERROR(LEPT_PARSE_ROOT_NOT_SINGULAR,"null      x");
+}
+static void test_parse_null() 
+{
     lept_value v;
-    v.type = LEPT_FALSE;
+    v.type = LEPT_FALSE;TESTERROR(LEPT_PARSE_ROOT_NOT_SINGULAR,"null    x");
     EXPECT_EQ_INT(LEPT_PARSE_OK, lept_parse(&v, "null"));
     EXPECT_EQ_INT(LEPT_NULL, lept_get_type(&v));
 }
-static void test_parse_expect_value()
+static void test_parse_true() 
 {
     lept_value v;
-    v.type=LEPT_FALSE;
-    EXPECT_EQ_INT(LEPT_PARSE_EXPECT_VALUE,lept_parse(&v,""));
-    EXPECT_EQ_INT(LEPT_NULL,lept_get_type(&v));
-
-    v.type=LEPT_FALSE;
-    EXPECT_EQ_INT(LEPT_PARSE_EXPECT_VALUE,lept_parse(&v," "));
-    EXPECT_EQ_INT(LEPT_NULL,lept_get_type(&v));
+    v.type = LEPT_FALSE;
+    EXPECT_EQ_INT(LEPT_PARSE_OK, lept_parse(&v, "true"));
+    EXPECT_EQ_INT(LEPT_TRUE, lept_get_type(&v));
 }
-static void test_parse_invalid_valie()
+
+static void test_parse_false() 
 {
     lept_value v;
-    v.type=LEPT_FALSE;
-    EXPECT_EQ_INT(LEPT_PARSE_INVALID_VALUE,lept_parse(&v,"nul"));
-    EXPECT_EQ_INT(LEPT_NULL,lept_get_type(&v));
-     
-    v.type=LEPT_FALSE;
-    EXPECT_EQ_INT(LEPT_PARSE_INVALID_VALUE,lept_parse(&v,"?"));
-    EXPECT_EQ_INT(LEPT_NULL,lept_get_type(&v));
+    v.type = LEPT_TRUE;
+    EXPECT_EQ_INT(LEPT_PARSE_OK, lept_parse(&v, "false"));
+    EXPECT_EQ_INT(LEPT_FALSE, lept_get_type(&v));
 }
-static void test_pares_root_not_singular()
-{
-    lept_value v;
-    v.type=LEPT_FALSE;
-    EXPECT_EQ_INT(LEPT_PARSE_ROOT_NOT_SINGULAR,lept_parse(&v,"null x"));
-    EXPECT_EQ_INT(LEPT_NULL,lept_get_type(&v));
 
-    v.type=LEPT_FALSE;
-    EXPECT_EQ_INT(LEPT_PARSE_ROOT_NOT_SINGULAR,lept_parse(&v,"null         x"));
-    EXPECT_EQ_INT(LEPT_NULL,lept_get_type(&v));
-
-    v.type=LEPT_FALSE;
-    EXPECT_EQ_INT(LEPT_PARSE_OK,lept_parse(&v,"null        "));
-    EXPECT_EQ_INT(LEPT_NULL,lept_get_type(&v));
-}
 static void test_parse()
 {
- //   test_parse_null();
-    test_parse_expect_value();
-    test_parse_invalid_valie();
-    test_pares_root_not_singular();
+    test_parse_null();
+    test_parse_true();
+    test_parse_false();
+    test_parse_error();
 }
 int main() {
     test_parse();
