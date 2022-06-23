5bc47a71fd1dd6fecaec07d94cd3b8edf31ed6c5
gueFDF <3237455241@qq.com>
2022-06-23 21:11:41 +0800

    第三单元

--- test.c
+++ test.c
@@ -19,8 +19,9 @@ static int test_pass=0;
 
 #define EXPECT_EQ_INT(expect,actual) EXPECT_EQ_BASE(((expect)==(actual)),expect,actual,"%d")
 #define EXPECT_EQ_DOUBLE(expect,actual) EXPECT_EQ_BASE(((expect)==(actual)),expect,actual,"%.17g")
-
-
+#define EXPECT_EQ_STRING(expect,actual,alength) EXPECT_EQ_BASE(sizeof(expect)-1==(alength)&& memcmp(expect,actual,alength)==0,expect,actual,"%s")
+#define EXPECT_EQ_TRUE(actual)  EXPECT_EQ_BASE(actual!=0,"true","false","%s")
+#define EXPECT_EQ_FALSE(atual)  EXPECT_EQ_BASE(actual==0,"false","true","%s")
 
 #define TESTERROR(error,json)\
     do{\
@@ -85,7 +86,15 @@ static void test_parse_false()
     EXPECT_EQ_INT(LEPT_FALSE, lept_get_type(&v));
 }
 
-
+static void test_access_string() {
+    lept_value v;
+    lept_init(&v);
+    lept_set_string(&v, "", 0);
+    EXPECT_EQ_STRING("", lept_get_string(&v), lept_get_string_length(&v));
+    lept_set_string(&v, "Hello", 5);
+    EXPECT_EQ_STRING("Hello", lept_get_string(&v), lept_get_string_length(&v));
+    lept_free(&v);
+}
 static void test_parse_number() {
     TEST_NUMBER(0.0, "0");
     TEST_NUMBER(0.0, "-0");
@@ -125,6 +134,7 @@ static void test_parse()
     test_parse_false();
     test_parse_number();
     test_parse_error();
+    test_access_string();
 }
 int main() {
     test_parse();
