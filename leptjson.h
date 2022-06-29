0bfa8c4450ce0b79355fe7fbeb4ff99be5935204
gueFDF <3237455241@qq.com>
2022-06-29 21:23:33 +0800

    第六单元

--- leptjson.h
+++ leptjson.h
@@ -15,7 +15,7 @@ typedef struct
 }lept_context;
 //枚举出来6中数据类型
 typedef enum { LEPT_NULL, LEPT_FALSE, LEPT_TRUE, LEPT_NUMBER, LEPT_STRING, LEPT_ARRAY, LEPT_OBJECT } lept_type;
-//枚举出来返回值
+//枚举出来返回码
 enum {
     LEPT_PARSE_OK = 0,     //正确
     LEPT_PARSE_EXPECT_VALUE,   //若一个 JSON 只含有空白
@@ -26,22 +26,30 @@ enum {
     LEPT_PARSE_INVALID_STRING_ESCAPE,
     LEPT_PARSE_INVALID_STRING_CHAR,
     LEPT_PARSE_INVALID_UNICODE_HEX,
-    LEPT_PARSE_INVALID_UNICODE_SURROGATE
+    LEPT_PARSE_INVALID_UNICODE_SURROGATE,
+    LEPT_PARSE_MISS_COMMA_OR_SQUARE_BRACKET,
+    LEPT_PARSE_MISS_KEY,
+    LEPT_PARSE_MISS_COLON,
+    LEPT_PARSE_MISS_COMMA_OR_CURLY_BRACKET
 };
 #define STRING_ERROR(ret) do { c->top = head; return ret; } while(0)
-typedef struct
-{
-    lept_type type;  //类型
-    union 
-    {
-        double n;  //数字(type == LEPT_NUMBER)
-        struct
-        {
-            char*s;
-            size_t len;
-        }s;
+typedef struct lept_value lept_value;
+typedef struct lept_member lept_member;
+
+struct lept_value {
+    union {
+        struct { lept_member* m; size_t size; }o;
+        struct { lept_value* e; size_t size; }a;
+        struct { char* s; size_t len; }s;
+        double n;
     }u;
-}lept_value; 
+    lept_type type;
+};
+
+struct lept_member {
+    char* k; size_t klen;   /* member key string, key string length */
+    lept_value v;           /* member value */
+};
 int lept_parse(lept_value* v, const char* json);   //解析json
 lept_type lept_get_type(const lept_value* v); //获取访问结果
 double lept_get_number(const lept_value* v);//获取数字
@@ -49,7 +57,7 @@ static int lept_parse_number(lept_context* c, lept_value* v);
 static int lept_parse_literal(lept_context* c, lept_value* v, const char* literal, lept_type type) ;
 void lept_set_string(lept_value* v, const char* s, size_t len);
 void lept_free(lept_value* v);
-
+static int lept_parse_value(lept_context* c, lept_value* v);
 #define lept_set_null(v) lept_free(v)
 #define lept_init(v)  do{(v)->type=LEPT_NULL;}while(0)
 int lept_get_boolean(const lept_value* v);
@@ -64,4 +72,16 @@ size_t lept_get_string_length(const lept_value* v);
 void lept_set_string(lept_value* v, const char* s, size_t len);
 static const char* lept_parse_hex4(const char* p, unsigned* u);
 static void lept_encode_utf8(lept_context* c, unsigned u);
+size_t lept_get_array_size(const lept_value*v);
+lept_value* lept_get_array_element(const lept_value*v,size_t index);
+static int lept_parse_array(lept_context*c,lept_value*v);
+
+
+static void*lept_context_pop(lept_context*c,size_t size);
+static void*lept_context_push(lept_context*c,size_t size);
+
+static int lept_parse_string_raw(lept_context* c, char** str, size_t* len) ;
+
+
+static int lept_parse_object(lept_context* c, lept_value* v);
 #endif 
