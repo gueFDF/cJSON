608a87564d8f237be57d41ab66a16b5239239274
gueFDF <3237455241@qq.com>
2022-07-09 13:36:53 +0800

    优化了字符串反解析,用空间换时间

--- leptjson.c
+++ leptjson.c
@@ -457,38 +457,44 @@ int lept_stringify(const lept_value* v, char** json, size_t* length) {
 #define PUTS(c, s, len)     memcpy(lept_context_push(c, len), s, len)
 static void lept_stringify_string(lept_context* c, const char* s, size_t len)
 {
-    size_t i;
+    size_t i,size;
+    char*head,*p;
     assert(s!=NULL);
-    PUTC(c,'"');
+    p=head=lept_context_push(c,size=len*6+2);
+    *p++='"';
+    static const char hex_digits[] = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F' };
     for(i=0;i<len;i++)
     {
         unsigned char ch=(unsigned char)s[i];
         switch(ch)
         {
-            case '\"': PUTS(c,"\\\"",2); break;
-            case '\\': PUTS(c,"\\\\",2); break;
-            case '\b': PUTS(c,"\\b",2);  break;
-            case '\f': PUTS(c,"\\f",2);  break;
-            case '\n': PUTS(c,"\\n",2);  break;
-            case '\r': PUTS(c,"\\r",2);  break;
-            case '\t': PUTS(c,"\\t",2);  break;
-
+            case '\"': *p++ = '\\'; *p++ = '\"'; break;
+            case '\\': *p++ = '\\'; *p++ = '\\'; break;
+            case '\b': *p++ = '\\'; *p++ = 'b';  break;
+            case '\f': *p++ = '\\'; *p++ = 'f';  break;
+            case '\n': *p++ = '\\'; *p++ = 'n';  break;
+            case '\r': *p++ = '\\'; *p++ = 'r';  break;
+            case '\t': *p++ = '\\'; *p++ = 't';  break;
 
             
             default:
             {
                 if(ch<0x20)
                 {
-                    char buf[7];
-                    sprintf(buf, "\\u%04X", ch);
-                    PUTS(c,buf,6);
+                    *p++='\\';
+                    *p++='u';
+                    *p++='0';
+                    *p++='0';
+                    *p++=hex_digits[ch>>4];
+                    *p++=hex_digits[ch&15];
                 }
                 else 
-                    PUTC(c,s[i]);
+                    *p++=ch;
             }
         }
     }
-    PUTC(c,'"');
+    *p++='"';
+    c->top-=size-(p-head);
 }
 static int lept_stringify_value(lept_context* c, const lept_value* v) {
     size_t i;
